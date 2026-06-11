f908e7255a6fba5269f17eb79dceb1e9ecc4faab
Agalya A <agalyahan07@gmail.com>
2026-06-11 23:09:28 +0530

    Create Main.java

---
+++ Module 1 (Design patterns & principles)/Exercise 1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+        logger1.log("Logging Message");
+        if(logger1 == logger2) {
+            System.out.println("Only One Instance Created");
+        }
+    }
+}
