2ce2db227fa78f11e50e65875ef9476bd4b26c87
Agalya A <agalyahan07@gmail.com>
2026-06-14 11:07:43 +0530

    Create FinancialForecasting.java

---
+++ Module 2 (Algorithms_Data structures)/FinancialForecasting.java
@@ -0,0 +1,14 @@
+public class FinancialForecasting {
+    static double forecast(double value, double growthRate, int years) {
+        if (years == 0)
+            return value;
+        return forecast(value * (1 + growthRate), growthRate, years - 1);
+    }
+    public static void main(String[] args) {
+        double currentValue = 10000;
+        double growthRate = 0.10;
+        int years = 5;
+        double futureValue = forecast(currentValue, growthRate, years);
+        System.out.println("Future Value: " + futureValue);
+    }
+}
