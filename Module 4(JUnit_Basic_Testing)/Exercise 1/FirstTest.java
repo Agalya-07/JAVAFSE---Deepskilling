7d1274290bf6d44bda45a9b0b9c26968f37cf8bb
Agalya A <agalyahan07@gmail.com>
2026-06-17 20:32:09 +0530

    Create FirstTest.java

---
+++ Module 4(JUnit_Basic_Testing)/Exercise 1/FirstTest.java
@@ -0,0 +1,9 @@
+package com.junit;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class FirstTest {
+    @Test
+    public void sampleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
