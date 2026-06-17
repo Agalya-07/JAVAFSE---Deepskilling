24b048aea5fb7bac03d2b7d32c92898089c5457a
Agalya A <agalyahan07@gmail.com>
2026-06-17 20:50:04 +0530

    Delete Module 4(JUnit_Basic_Testing)/Exercise 1 directory

--- Module 4(JUnit_Basic_Testing)/Exercise 1/FirstTest.java
+++
@@ -1,9 +0,0 @@
-package com.junit;
-import org.junit.Test;
-import static org.junit.Assert.*;
-public class FirstTest {
-    @Test
-    public void sampleTest() {
-        assertEquals(4, 2 + 2);
-    }
-}
