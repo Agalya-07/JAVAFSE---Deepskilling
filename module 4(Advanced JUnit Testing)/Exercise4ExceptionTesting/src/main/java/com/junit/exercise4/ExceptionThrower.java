5343d5a21f132c4656d3ce288c65eef5dc4aaa84
Agalya A <agalyahan07@gmail.com>
2026-06-20 10:13:25 +0530

    Add files via upload

---
+++ module 4(Advanced JUnit Testing)/Exercise4ExceptionTesting/src/main/java/com/junit/exercise4/ExceptionThrower.java
@@ -0,0 +1,6 @@
+package com.junit.exercise4;
+public class ExceptionThrower {
+    public void throwException() {
+        throw new ArithmeticException("Exception Occurred");
+    }
+}
---
+++ module 4(Advanced JUnit Testing)/Exercise4ExceptionTesting/src/test/java/com/junit/exercise4/ExceptionThrowerTest.java
@@ -0,0 +1,13 @@
+package com.junit.exercise4;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+public class ExceptionThrowerTest {
+    ExceptionThrower obj = new ExceptionThrower();
+    @Test
+    void testException() {
+        assertThrows(ArithmeticException.class, () -> {
+            obj.throwException();
+        });
+
+    }
+}
