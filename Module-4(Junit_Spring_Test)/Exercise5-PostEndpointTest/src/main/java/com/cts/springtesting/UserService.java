7b5651e9337e41454cd36ebe807d4d70e9a1e781
Agalya A <agalyahan07@gmail.com>
2026-06-21 15:11:18 +0530

    Add files via upload

---
+++ Module-4(Junit_Spring_Test)/Exercise5-PostEndpointTest/src/main/java/com/cts/springtesting/UserService.java
@@ -0,0 +1,11 @@
+package com.cts.springtesting;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public User saveUser(User user) {
+        return user;
+    }
+}
---
+++ Module-4(Junit_Spring_Test)/Exercise5-PostEndpointTest/src/test/java/com/cts/springtesting/UserControllerPostTest.java
@@ -0,0 +1,37 @@
+package com.cts.springtesting;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+public class UserControllerPostTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void testCreateUser() throws Exception {
+
+        User user = new User(1L, "John");
+
+        when(userService.saveUser(any(User.class))).thenReturn(user);
+
+        mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\":1,\"name\":\"John\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("John"));
+    }
+}
