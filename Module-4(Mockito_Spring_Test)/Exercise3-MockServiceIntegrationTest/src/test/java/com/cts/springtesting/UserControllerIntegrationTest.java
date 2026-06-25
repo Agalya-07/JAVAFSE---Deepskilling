7e7ef2757e9693c05e40280ecb2d3561c100d6e1
Agalya A <agalyahan07@gmail.com>
2026-06-25 09:50:45 +0530

    Add files via upload

---
+++ Module-4(Mockito_Spring_Test)/Exercise3-MockServiceIntegrationTest/src/test/java/com/cts/springtesting/UserControllerIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.cts.springtesting;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void testGetUser() throws Exception {
+
+        when(userService.getUserById(1L))
+                .thenReturn(new User(1L, "John"));
+
+        mockMvc.perform(get("/users/1"))
+               .andExpect(status().isOk());
+    }
+}
