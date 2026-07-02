148e4caff54948668af0badad4ef13efd207d3a8
Agalya A <agalyahan07@gmail.com>
2026-07-02 23:40:55 +0530

    Add files via upload

---
+++ Module-7(Spring REST using Spring Boot 3)/1. spring-rest-handson/hands-on-1/src/main/java/com/cognizant/handson1/HandsOn1Application.java
@@ -0,0 +1,13 @@
+package com.cognizant.handson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOn1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOn1Application.class, args);
+	}
+
+}
