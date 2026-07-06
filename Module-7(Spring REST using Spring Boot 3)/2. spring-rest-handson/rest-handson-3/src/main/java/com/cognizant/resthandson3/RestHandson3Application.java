16ec699aadc9711bd20af0735063c86ddfcc44ab
Agalya A <agalyahan07@gmail.com>
2026-07-06 22:18:10 +0530

    Add files via upload

---
+++ Module-7(Spring REST using Spring Boot 3)/2. spring-rest-handson/rest-handson-3/src/main/java/com/cognizant/resthandson3/RestHandson3Application.java
@@ -0,0 +1,13 @@
+package com.cognizant.resthandson3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHandson3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHandson3Application.class, args);
+	}
+
+}
---
+++ Module-7(Spring REST using Spring Boot 3)/2. spring-rest-handson/rest-handson-3/src/main/java/com/cognizant/resthandson3/controller/CountryController.java
@@ -0,0 +1,22 @@
+package com.cognizant.resthandson3.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.resthandson3.model.Country;
+import com.cognizant.resthandson3.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+}
