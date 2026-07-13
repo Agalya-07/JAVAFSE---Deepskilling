86f76c5e6b838723a3abcbb5cc3b18d92419ed48
Agalya A <agalyahan07@gmail.com>
2026-07-13 22:09:53 +0530

    Add files via upload

---
+++ Module-8(Microservices)/0. Sample Microservices exercises/exercise3-jwt-security/src/main/java/com/cognizant/Exercise3JwtApplication.java
@@ -0,0 +1,12 @@
+package com.cognizant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise3JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise3JwtApplication.class, args);
+    }
+}
---
+++ Module-8(Microservices)/0. Sample Microservices exercises/exercise3-jwt-security/src/main/java/com/cognizant/security/JwtTokenProvider.java
@@ -0,0 +1,34 @@
+package com.cognizant.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cognizant.config.JwtConfig;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtTokenProvider {
+
+    @Autowired
+    private JwtConfig jwtConfig;
+
+    public String createToken(String username) {
+
+        Claims claims = Jwts.claims().setSubject(username);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + 3600000);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, jwtConfig.getSecret())
+                .compact();
+    }
+}
