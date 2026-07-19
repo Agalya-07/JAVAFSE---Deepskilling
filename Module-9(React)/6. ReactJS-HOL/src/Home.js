e6c7b0db185178143b7f03c147f9146c35e152d5
Agalya A <agalyahan07@gmail.com>
2026-07-19 18:11:57 +0530

    Add files via upload

---
+++ Module-9(React)/6. ReactJS-HOL/src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Cognizant Academy Trainers Portal</h2>
+      <p>Use the navigation menu to view trainers list.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ Module-9(React)/6. ReactJS-HOL/src/TrainersMock.js
@@ -0,0 +1,38 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+    new Trainer(
+        1,
+        "John Doe",
+        "john@example.com",
+        "9876543210",
+        "Java",
+        "Spring Boot, React"
+    ),
+    new Trainer(
+        2,
+        "Emma Swan",
+        "emma@example.com",
+        "9876543211",
+        ".NET",
+        "ASP.NET Core, Angular"
+    ),
+    new Trainer(
+        3,
+        "Marie Curie",
+        "marie@example.com",
+        "9876543212",
+        "Python",
+        "Django, Flask"
+    ),
+    new Trainer(
+        4,
+        "Elisa Smith",
+        "elisa@example.com",
+        "9876543213",
+        "Java",
+        "Spring, Hibernate"
+    )
+];
+
+export default trainers;
